2ceb4cb65e86c42558e3fa3ab90a0a6cf1b5d2e0
Michael <107328015+Boys-Who-Cry@users.noreply.github.com>
2024-04-19 18:11:48 -0400

    Completed creation of account login and account creation components. But still not connected to a backend.

--- weather-react/src/components/CreateAccountForm.jsx
+++ weather-react/src/components/CreateAccountForm.jsx
@@ -3,22 +3,39 @@ import styles from "../css/main.module.css";
 const CreateAccountForm = () => {
   return (
     <>
-      <section className={styles.account__container}>
-        <section className={styles.account__container__header}>
-          <h2>Create Account</h2>
-        </section>
-        <section>
-          <form className={styles.account__container__form}>
-            <section className={styles.account__container__subgrid}>
+      {window.innerWidth < 641 ? (
+        <section className={styles.account__container}>
+          <section className={styles.account__container__header}>
+            <h2>Create Account</h2>
+          </section>
+          <section>
+            <form className={styles.account__container__form}>
               <input type="text" placeholder="Enter your first name" />
               <input type="text" placeholder="Enter your last name" />
-            </section>
-            <input type="email" placeholder="Enter your email address" />
-            <input type="password" placeholder="Create a password" />
-            <Button type="submit" buttonText="Create Account" />
-          </form>
+              <input type="email" placeholder="Enter your email address" />
+              <input type="password" placeholder="Create a password" />
+              <Button type="submit" buttonText="Create Account" />
+            </form>
+          </section>
         </section>
-      </section>
+      ) : (
+        <section className={styles.account__container}>
+          <section className={styles.account__container__header}>
+            <h2>Create Account</h2>
+          </section>
+          <section>
+            <form className={styles.account__container__form}>
+              <section className={styles.account__container__subgrid}>
+                <input type="text" placeholder="Enter your first name" />
+                <input type="text" placeholder="Enter your last name" />
+              </section>
+              <input type="email" placeholder="Enter your email address" />
+              <input type="password" placeholder="Create a password" />
+              <Button type="submit" buttonText="Create Account" />
+            </form>
+          </section>
+        </section>
+      )}
     </>
   );
 };
