66fefc80e1f161dd45b46a3dc1b687cd986b59e9
Michael <107328015+Boys-Who-Cry@users.noreply.github.com>
2024-04-25 23:11:57 -0400

    Created new layout on smaller devices and touched up css bugs as screen expands/contracts.

--- weather-react/src/pages/Home.jsx
+++ weather-react/src/pages/Home.jsx
@@ -1,22 +1,21 @@
 import { Link } from "react-router-dom";
 import FetchWeatherForm from "../components/FetchWeatherForm";
 import "../css/index.css";
+import home__styles from "../css/home_styles.module.css";
 import styles from "../css/main.module.css";
 const Home = () => {
   return (
     <>
       <main className="container">
-        <section className={styles.home__grid}>
-          <section>
-            <h1 className={styles.home__grid__header}>Weather</h1>
+        <section className={home__styles.home__grid}>
+          <section className={home__styles.home__grid__header__container}>
+            <h1>Weather</h1>
           </section>
-          <section className={styles.form}>
+          <section className={home__styles.home__grid__form__container}>
             <FetchWeatherForm doWeatherButton={true} />
           </section>
-          <section>
-            <p>
-              <Link to="account">Login or Sign Up</Link>
-            </p>
+          <section className={home__styles.home__grid__accountLink__container}>
+            <Link to="account">Login or Sign Up</Link>
           </section>
         </section>
       </main>
